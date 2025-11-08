187b231ff4a794c2a21b3b24b39f77484874ec74
ArinaBala <arinabalashova.od@gmail.com>
2025-11-08 16:57:11 +0200

    Update main.cpp

--- finalTask/main.cpp
+++ finalTask/main.cpp
@@ -9,26 +9,26 @@ int main() {
     Bus bus(20, 30, 20, 60);
     Truck truck(5, 10, 10, 60);
 
-    std::cout << "\nBus arrives...\n";
+    cout << "\nBus arrives...\n";
     bus.arrive();
     Base::printStatus();
 
-    std::cout << "\nTruck arrives...\n";
+    cout << "\nTruck arrives...\n";
     truck.arrive();
     Base::printStatus();
 
-    std::cout << "\nBus tries to leave...\n";
+    cout << "\nBus tries to leave...\n";
     if (bus.leave())
-        std::cout << "Bus left.\n";
+        cout << "Bus left.\n";
     else
-        std::cout << "Bus cannot leave.\n";
+        cout << "Bus cannot leave.\n";
     Base::printStatus();
 
     std::cout << "\nTruck tries to leave...\n";
     if (truck.leave())
-        std::cout << "Truck left.\n";
+        cout << "Truck left.\n";
     else
-        std::cout << "Truck cannot leave.\n";
+        cout << "Truck cannot leave.\n";
     Base::printStatus();
 
     return 0;
